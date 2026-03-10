03ff9ce89532ec49a2851d4d22ccae0c2711f702
Helena Silva <helenatchuda@gmail.com>
2026-03-10 13:25:58 +0000

    chances at routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,11 +7,10 @@ import { getAuthenticatedUser } from "../controllers/users.js";
 
 import { Router } from "express";
 export const usersRouter = Router();
-
-usersRouter.get("/:name", getUserByName);
-
-usersRouter.get("/:id", getUserById);
-usersRouter.get("/", getUsers);
-
+//primeiro vem as rotas com nome fixo
 usersRouter.get("/me", getAuthenticatedUser);
+usersRouter.get("/", getUsers);
+//depois vecm as rotas com o nome dinâmico
+usersRouter.get("/:name", getUserByName);
+usersRouter.get("/:id", getUserById);
 usersRouter.patch("/:id", updateUser);
