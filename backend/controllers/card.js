340ae1381174cc27edc24a88770ae4c2a28908ac
Helena Silva <helenatchuda@gmail.com>
2026-04-10 21:57:00 +0100

    button like and dislike

--- backend/controllers/card.js
+++ backend/controllers/card.js
@@ -1,110 +1,98 @@
-import{Card} from "../models/cards.js";
-import { UnauthorizedError } from '../erros/Unauthorize.js';
+import { Card } from "../models/cards.js";
+import { UnauthorizedError } from "../erros/Unauthorize.js";
 
-
-export async function getCards(req,res,next){
+export async function getCards(req, res, next) {
   console.log("Minha requisição:", req);
-  try{
+  try {
     const Cards = await Card.find({});
     res.status(200).json(Cards);
-  }catch(err){
+  } catch (err) {
     console.error("Erro ao buscar cartões:", err);
     next(err);
   }
 }
 
- export async function getCardById(req,res){
-  const card = Card.find(c=>c._id ===req.params.id)
-    if(!card){
-      throw new NotFoundError("ID do cartão não encontrado");
-
-    }
-    else{
-      res.status(200).json(card)
-    }
-
+export async function getCardById(req, res) {
+  const card = Card.find((c) => c._id === req.params.id);
+  if (!card) {
+    throw new NotFoundError("ID do cartão não encontrado");
+  } else {
+    res.status(200).json(card);
+  }
 }
-export async function createCard(req,res,next){
+export async function createCard(req, res, next) {
+  const body = req.body;
+  console.log(body.name);
 
-  const body = req.body
-     console.log(body.name);
-
-
-     try{
-      const cardCreated = await Card.create({
-
-        name: body.name,
-        link: body.link,
-        owner:req.user._id
-
-
-
-      });
-      console.log(cardCreated);
-      res.status(201).json(cardCreated);
-    }catch(err){
-      next (err);
-
-
-    }
+  try {
+    const cardCreated = await Card.create({
+      name: body.name,
+      link: body.link,
+      owner: req.user._id,
+    });
+    console.log(cardCreated);
+    res.status(201).json(cardCreated);
+  } catch (err) {
+    next(err);
+  }
 }
-export async function updateCard(req,res){
-  const card = Card.find(c=> c._id === req.params.id)
-    if(!card){
-      throw new NotFoundError("ID do cartão não encontrado");
-    }else{
-      const body = req.body
-      card.name = body.name || card.name
-      card.link = body.link || card.link
-      card.owner = body.owner || card.owner
+export async function updateCard(req, res) {
+  const card = Card.find((c) => c._id === req.params.id);
+  if (!card) {
+    throw new NotFoundError("ID do cartão não encontrado");
+  } else {
+    const body = req.body;
+    card.name = body.name || card.name;
+    card.link = body.link || card.link;
+    card.owner = body.owner || card.owner;
 
-      res.status(200).json(card)
-    }
+    res.status(200).json(card);
+  }
 }
- export async function likeCard(req,res,next){
-  const{cardId}= req.params;
+export async function likeCard(req, res, next) {
+  const { cardId } = req.params;
 
-  try{
+  try {
     const likedCard = await Card.findByIdAndUpdate(
-
-
       cardId,
-      {$addToSet:{likes:req.user._id}},
-      {new:true},
-    ).orFail(()=>{throw new NotFoundError("ID do cartão não encontrado")});
+      { $addToSet: { likes: req.user._id } },
+      { new: true },
+    ).orFail(() => {
+      throw new NotFoundError("ID do cartão não encontrado");
+    });
 
     res.status(200).json(likedCard);
-  }catch(err){
-    return next (err);
+  } catch (err) {
+    return next(err);
   }
 }
-export async function dislikeCard (req,res,next){
-  const{cardId}= req.params;
+export async function dislikeCard(req, res, next) {
+  const { cardId } = req.params;
 
-  try{
+  try {
     const dislikedCard = await Card.findByIdAndUpdate(
       cardId,
-      {$pull:{likes:req.user._id}},
-      {new:true},
-    ).orFail(()=>{throw new NotFoundError("ID do cartão não encontrado")});
+      { $pull: { likes: req.user._id } },
+      { new: true },
+    ).orFail(() => {
+      throw new NotFoundError("ID do cartão não encontrado");
+    });
 
     res.status(200).json(dislikedCard);
-  }catch(err){
-    return next (err);
+  } catch (err) {
+    return next(err);
   }
 }
-export async function deleteCard(req,res,next){
- const{id}=req.params
- console.log("ID to delete:", id);
+export async function deleteCard(req, res, next) {
+  const { id } = req.params;
+  console.log("ID to delete:", id);
 
- try{
-  await Card
-   .findByIdAndDelete(id)
-   .orFail(()=>{throw new NotFoundError("ID do cartão não encontrado")})
-
- }catch(err){
-  return next (err);
- }
- res.status(200).json({message:"Cartão deletado com sucesso"})
-
+  try {
+    await Card.findByIdAndDelete(id).orFail(() => {
+      throw new NotFoundError("ID do cartão não encontrado");
+    });
+  } catch (err) {
+    return next(err);
+  }
+  res.status(200).json({ message: "Cartão deletado com sucesso" });
 }
